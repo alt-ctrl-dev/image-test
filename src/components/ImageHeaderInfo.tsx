1bc52d57ce4d4b65bc6d9937498df720fbbd7725
Reuben <reuben@githubt.com>
2025-07-20 20:30:05 +0800

    update image header info

--- src/components/ImageHeaderInfo.tsx
+++ src/components/ImageHeaderInfo.tsx
@@ -3,24 +3,32 @@ import useSWR from "swr";
 async function fetchImage(request: string) {
     try {
         const response = await fetch(request);
-        response.headers.forEach((value, key) => {
-            if(key.toLowerCase() === "content-encoding") 
-            console.log(`${key}: ${value}`);
-        })
         const contentEncoding = response.headers.get("content-encoding");
         const contentLength = response.headers.get("content-length");
-        return [contentEncoding??"No encoding", contentLength??"No length"].join(" | ");
+        return [contentEncoding ?? "No encoding", contentLength ?? "No length"];
     } catch (error) {
         console.error("Error:", error);
-        return "Error";
+        throw error;
     }
 }
 
-const ImageHeaderInfo = ({src}:{src:string}) =>{
-    const { data, error, isLoading } = useSWR(src, fetchImage)
-    if (error) return <div>failed to load</div>
-    if (isLoading) return <div>loading...</div>
-    return <div>{data}</div>
+const ImageHeaderInfo = ({ src }: { src: string }) => {
+    const { data, error, isLoading } = useSWR(src, fetchImage,  { 
+        refreshInterval: 0,
+        errorRetryCount: 1, 
+        revalidateIfStale: false, 
+        revalidateOnFocus: false, 
+        revalidateOnReconnect: false,
+        revalidateOnMount: false
+     })
+    if (error) return <td colSpan={2}>failed to load</td>
+    if (isLoading) return <td colSpan={2}>loading...</td>
+    if (!data) return <td colSpan={2}>No data</td>
+    return <>{
+        data.map((item, index) => {
+            return <td key={index}>{item}</td>
+        })
+    }</>
 }
 
 export default ImageHeaderInfo;
