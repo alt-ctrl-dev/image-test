350f6708a0197011c2452cd9ed44f3299dfe5719
Reuben <reuben@githubt.com>
2025-07-20 20:02:39 +0800

    remove inline asset

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/image-test/"
+  base: "/image-test/",
+  build:{
+    assetsInlineLimit: 0, // Disable inlining of assets
+  }
 })
